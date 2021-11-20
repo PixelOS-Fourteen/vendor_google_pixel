7f7b3d092a9fbe50880e501c2b3bf7be7ec2dfa9
AdarshGrewal <adarshgrewal@gmail.com>
2021-11-20 18:36:59 +0530

    pixel: Import ime props
    * taken from Pixel 6 Pro
    
    Change-Id: Id5338734d3e4492d1b3da3d47a0430b102086d4e

--- config.mk
+++ config.mk
@@ -19,6 +19,12 @@ $(call inherit-product, vendor/aospa/prebuilt/pixel.mk)
 # Fonts
 include vendor/google/pixel/fonts/fonts.mk
 
+# Gboard
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.ime.bs_theme=true \
+    ro.com.google.ime.theme_id=5 \
+    ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms
+
 # Overlays
 PRODUCT_PACKAGES += \
     PixelDocumentsUIGoogleOverlay \
